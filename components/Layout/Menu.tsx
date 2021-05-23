4c97c8137515c363a92eeda3b70120d6c44ca68c
movwf <movwf@protonmail.com>
2021-05-23 09:51:03 +0300

    .env Added

--- components/Layout/Menu.tsx
+++ components/Layout/Menu.tsx
@@ -6,7 +6,9 @@ function Menu() {
       aria-label="Main"
       className="flex-1 px-2 py-4 space-y-2 overflow-y-hidden hover:overflow-y-auto"
     >
-      <MenuItem />
+      <ul className="list-none">
+        <MenuItem name="Home" icon="home" href="/" />
+      </ul>
     </nav>
   );
 }
--- components/Layout/Sidebar.tsx
+++ components/Layout/Sidebar.tsx
@@ -3,7 +3,7 @@ import SidebarButton from './SidebarButton';
 
 function Sidebar() {
   return (
-    <aside className="flex-shrink-0 hidden w-64 bg-white border-r dark:border-primary-darker dark:bg-darker md:block">
+    <aside className="flex-shrink-0 hidden w-16 bg-white border-r dark:border-primary-darker dark:bg-darker md:block">
       <div className="flex flex-col h-full">
         <Menu />
         <SidebarButton />
